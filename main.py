70febfe077f877e3709e802880219873a0387182
DexenD1 <desendoimanuel46@gmail.com>
2021-06-03 01:46:53 +0700

    Refined Code
    Remove unused google.cloud.storage library

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import io, os, tempfile
+import requests, json
 # Import standart google service library
-from google.cloud import storage, vision, firestore
+from google.cloud import vision, firestore
 
 # Creates a client
-gcsClient = storage.Client()
 visionClient = vision.ImageAnnotatorClient()
 firestoreClient = firestore.Client()
 
@@ -15,29 +14,26 @@ def gcsObjectChanges(event, context):
     """
     file = event
 
+    # Get the recent uploaded image from Google Storage Bucket
     # Define image file name and bucket name
     fileName = file["name"]
     bucketName = file["bucket"]
-    print(f"FILE NAME: {fileName}")
-    print(f"BUCKET NAME: {bucketName}")
 
-    # Get the recent uploaded image from Google Storage Bucket
-    blobFile = gcsClient.bucket(bucketName).get_blob(fileName)
-    blobFileUri = f"gs://{bucketName}/{fileName}"
-    print(f"BLOB FILE: {blobFile}")
-    print(f"BLOB FILE URI: {blobFileUri}")
-    blobImageFile = vision.Image(source=vision.ImageSource(image_uri=blobFileUri))
+    # Send a POST request to Machine Learning Instance through an API endpoint
+    # Analyze the recent uploaded image in the Google Cloud Storage
+    url = 'http://EXTERNAL_IP_API_INSTANCE/analyze'
+    analyzeRequest = requests.post(
+        url + 
+        "?uri=gs://" + 
+        bucketName + 
+        "/&fileName=" + 
+        fileName
+    )
+    requestDict = json.loads(analyzeRequest.text)
+    information = requestDict["message"]["information"]
 
-    # Perform label detection
-    result = visionClient.label_detection(image=blobImageFile)
-    labels = result.label_annotations
-    information = []
-    print("LABELS:")
-    for label in labels:
-        print(label.description)
-        information.append(label.description)
-
-    # Post the image name and labels as information to Firestore
+    # Post the image name and labels as an data information in Firestore
+    # Store data in the 'images' collection and use the file name as the document ID
     documentReference = firestoreClient.collection(u"images").document(fileName)
     documentReference.set(
         {
